4279a94e21c9beeba7484b338c44e12d003ea6d1
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-06-05 21:08:39 +0700

    NEAR FULL 2

--- src/main/java/com/thanhtan/groceryshop/entity/User.java
+++ src/main/java/com/thanhtan/groceryshop/entity/User.java
@@ -19,6 +19,7 @@ import java.util.Set;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
+@Table(name="users")
 public class User extends BaseEntity {
 
     String username;
